1376fce10914c99f670efc3a45206ac258d80ae0
RawiriHohepa <54158903+RawiriHohepa@users.noreply.github.com>
2019-08-17 15:43:08 +1200

    Added logo

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 3000;
 var router = express.Router();
 var path = __dirname + '/views/';
 
+app.use(express.static(__dirname+'/public'));
+
 router.use(function (req,res,next) {
     console.log("/" + req.method);
     next();
